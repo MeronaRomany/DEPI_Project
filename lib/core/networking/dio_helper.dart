04692fdc82d953efdb864bac398f996900df4f15
MeronaRomany <152447527+MeronaRomany@users.noreply.github.com>
2026-03-12 20:26:59 +0200

    init project

---
+++ lib/core/networking/dio_helper.dart
@@ -0,0 +1,27 @@
+import 'package:depi_project/core/networking/api_constant.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class DioHelper {
+ static late Dio dio;
+  static   init(){
+   dio= Dio(BaseOptions(
+      baseUrl: ApiConstant.baseUrl,
+      receiveDataWhenStatusError: true,
+      connectTimeout: Duration(seconds: 10),
+      receiveTimeout: Duration(seconds: 10),
+      headers: {
+        "X-RapidAPI-Key":dotenv.env["API_KEY"],
+        "X-RapidAPI-Host":"travel-advisor.p.rapidapi.com",
+        "Content-Type":"application/json"
+      }
+    ));
+  }
+
+  static Future<Response> getData({required String endPoint,required Map<String,dynamic>? queryParameters})async{
+
+   var response= await dio.get(endPoint, queryParameters:queryParameters);
+   return response;
+  }
+
+}
